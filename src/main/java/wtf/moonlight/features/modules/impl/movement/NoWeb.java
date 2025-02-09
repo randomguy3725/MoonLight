8e9f9fa97f291fd235b9d1595c3240303fe6d30d
eonianisnoob <158603026+eonianisnoob@users.noreply.github.com>
2025-02-09 17:59:20 +0200

    fix(?)

--- src/main/java/wtf/moonlight/features/modules/impl/movement/NoWeb.java
+++ src/main/java/wtf/moonlight/features/modules/impl/movement/NoWeb.java
@@ -22,7 +22,7 @@ public class NoWeb extends Module {
 
     private final ModeValue mode = new ModeValue("Mode", new String[]{"Vanilla", "GrimAC", "Intave"}, "Vanilla",this);
     public final BoolValue upAndDown = new BoolValue("Up And Down",true,this,() -> mode.is("Intave"));
-    public final BoolValue groundBoost = new BoolValue("Ground Boost",true,this,() -> mode.is("Intave"));
+    public final BoolValue jump = new BoolValue("Jump",true,this,() -> mode.is("Intave"));
     public final BoolValue noDown = new BoolValue("No Down",true,this,() -> mode.is("Intave"));
 
     @EventTarget
@@ -50,9 +50,9 @@ public class NoWeb extends Module {
                 searchBlock = PlayerUtils.searchBlocks(2);
                 for (Map.Entry<BlockPos, Block> block : searchBlock.entrySet()) {
                     if (mc.theWorld.getBlockState(block.getKey()).getBlock() instanceof BlockWeb) {
-                        if (mc.thePlayer.onGround) {
-                            mc.thePlayer.jump();
-                            MovementUtils.moveFlying(groundBoost.get() ? 0.4 : 0.3);
+                        if (jump.get())
+                            if (mc.thePlayer.onGround) {
+                                mc.thePlayer.jump();
                         } else if (noDown.get()) {
                             if (upAndDown.get())
                                 if (mc.gameSettings.keyBindSneak.isKeyDown())
