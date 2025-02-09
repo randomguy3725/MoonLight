2bce15dfb4d3e9f4f9ef35061346ee561bdbcc72
Randumbguy <110034079+randomguy3725@users.noreply.github.com>
2025-02-09 15:44:34 +0800

    tf
    Signed-off-by: Randumbguy <110034079+randomguy3725@users.noreply.github.com>

--- src/main/java/wtf/moonlight/features/modules/impl/movement/Strafe.java
+++ src/main/java/wtf/moonlight/features/modules/impl/movement/Strafe.java
@@ -23,13 +23,11 @@ import wtf.moonlight.utils.player.MovementUtils;
 public class Strafe extends Module {
 
     public final BoolValue ground = new BoolValue("Ground", true, this);
-    public final SliderValue groundSpeed = new SliderValue("Ground Speed", 1, 0.01f, 10f, 0.1f, this);
     public final BoolValue air = new BoolValue("Air", true, this);
-    public final SliderValue airSpeed = new SliderValue("Air Speed", 1, 0.01f, 10f, 0.1f, this);
 
     @EventTarget
     public void onUpdate(UpdateEvent event) {
-        if (mc.thePlayer.onGround && ground.get()) MovementUtils.strafe(groundSpeed.get());
-        if (!mc.thePlayer.onGround && air.get()) MovementUtils.strafe(airSpeed.get());
+        if (mc.thePlayer.onGround && ground.get()) MovementUtils.strafe();
+        if (!mc.thePlayer.onGround && air.get()) MovementUtils.strafe();
     }
 }
