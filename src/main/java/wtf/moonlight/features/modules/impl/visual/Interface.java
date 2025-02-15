1f3a2aebd60434f9cd31d5fbce0a3e0aaca8942f
eonianisnoob <158603026+eonianisnoob@users.noreply.github.com>
2025-02-15 09:00:06 +0200

    Update Interface.java

--- src/main/java/wtf/moonlight/features/modules/impl/visual/Interface.java
+++ src/main/java/wtf/moonlight/features/modules/impl/visual/Interface.java
@@ -430,6 +430,7 @@ public class Interface extends Module {
                         if (event.getShaderType() == Shader2DEvent.ShaderType.GLOW) {
                             RenderUtils.drawRect(leftSide, y, moduleWidth + 3, bottom, color(count));
                         }
+                    }
 
                     if (line.is("Left")) {
                         RenderUtils.drawRect(leftSide - 1, (float) translate.getY(), 1, bottom, color(count));
